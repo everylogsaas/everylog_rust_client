fc6745bc8339e58053b3ca16b076d103b4d79503
David Gasana Manzi <gmdavid59@gmail.com>
2023-06-15 22:57:08 +0200

    client unit test

---
+++ tests/everylog_rust_client_test.rs
@@ -0,0 +1,46 @@
+use everylog_rust_client::EveryLogRustClient;
+use serde_json::json;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{mock, Matcher};
+
+    #[test]
+    fn test_notification() {
+        // Start a mock server and get the URL
+        let server_url = mockito::server_url();
+
+        // Create a mock for the post request
+        let mock = mock("POST", "/api/v1/log-entries")
+            .match_header("Content-Type", "application/json")
+            .match_header("Authorization", Matcher::Regex(r"^Bearer \w+$".to_string()))
+            .with_status(200)
+            .with_body(r#"{"success": true}"#)
+            .create();
+
+        let mut client = EveryLogRustClient::new();
+        client.setup(Some(json!({
+            "api_key": "your_api_key",
+            "projectId": "your_project_id",
+            "everylog_url": server_url + "/api/v1/log-entries",
+        })));
+
+        let response = client.notify(Some(json!({
+            "title": "Notification Title",
+            "summary": "Notification Summary",
+            "body": "Notification Body"
+        })));
+
+        match response {
+            Ok(result) => {
+                assert_eq!(result, json!({"success": true}));
+                println!("Notification sent successfully: {:?}", result);
+            }
+            Err(err) => eprintln!("Error sending notification: {:?}", err),
+        }
+
+        // Assert that the mock request was called
+        mock.assert();
+    }
+}
